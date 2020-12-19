4dbc5ece7f2cd4a395be78b35ce72d75eec90844
Manas Mishra <manas1.mishra@ril.com>
2020-12-19 16:55:23 +0530

    mock data added

---
+++ PayU_TestTests/mock/MockDataGet.swift
@@ -0,0 +1,8 @@
+//
+//  MockDataGet.swift
+//  PayU_TestTests
+//
+//  Created by Manas1 Mishra on 19/12/20.
+//
+
+import Foundation
---
+++ PayU_TestTests/mock/MockNetworkManager.swift
@@ -0,0 +1,17 @@
+//
+//  MockNetworkManager.swift
+//  PayU_TestTests
+//
+//  Created by Manas1 Mishra on 19/12/20.
+//
+
+import Foundation
+@testable import PayU_Test
+
+class MockNetworkManager: NetworkManagerProtocol {
+    func doNetworkCallForMovieList(searchKey: String, pageNo: Int, totalPage: Int, completion: @escaping NetworkManager.Completion) {
+        
+    }
+    
+    
+}
