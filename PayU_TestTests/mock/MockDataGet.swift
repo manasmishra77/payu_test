231a09c68f569f9088d1c280e5904d954df654c1
Manas Mishra <manas1.mishra@ril.com>
2020-12-19 17:00:34 +0530

    json loader added

--- PayU_TestTests/mock/MockDataGet.swift
+++ PayU_TestTests/mock/MockDataGet.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+enum ModelJSONPath: String {
+    case movielistpage1 = "MovieListResult_Page1"
+    case movielistpage2 = "MovieResultPage_2"
+    case movielistBlankContent = "MovieResultBlankContent"
+    case movielistNewContent = "MovieResultNewContent"
+}
+
+func loadModel<T: Decodable>(_ fileName: String, as type: T.Type = T.self, bundle: Bundle) -> T {
+    let data: Data
+    
+    //let url = bundle.url(forResource: fileName, withExtension: "json")
+    
+    
+    guard let file = bundle.url(forResource: fileName, withExtension: "json") else {
+        fatalError("Couldn't find \(fileName) in bundle")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch  {
+        fatalError("Couldn't find \(fileName) in bundle with error: \(error)")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch  {
+        print(error)
+        fatalError("Couldn't find \(fileName) in bundle as \(T.self) with error: \(error)")
+    }
+}
